66cbabb09f67e175aacec8c4f4e9fac7c4d7a2c5
Leandro Ferreira <leandro.daf@gmail.com>
2024-10-26 08:42:34 -0300

    feat(channel): add MIDIEventPublisher interface for MIDI event management
    Define an interface for publishing MIDI events, retrieving the event channel, and handling channel shutdown.

---
+++ internal/contracts/channel/channel.go
@@ -0,0 +1,13 @@
+package channel
+
+import (
+	"github.com/leandrodaf/midi-client/internal/entity"
+)
+
+// MIDIEventPublisher defines an interface for publishing and managing MIDI events.
+type MIDIEventPublisher interface {
+	Publish(event entity.MIDI)  // Publishes a MIDI event to the channel.
+	Events() <-chan entity.MIDI // Returns the MIDI event channel for reading.
+	Shutdown()                  // Shuts down the event channel.
+	GetChannel() chan entity.MIDI
+}
