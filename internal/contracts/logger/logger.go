117b331f3d64667774c87755b93f15c81c6a88e5
Leandro Ferreira <leandro.daf@gmail.com>
2024-10-26 08:43:44 -0300

    feat(logger): add logger interface and field options with basic documentation
    Provide logging interface for different levels and field types, including configuration for production and development environments.

--- internal/contracts/logger/logger.go
+++ internal/contracts/logger/logger.go
@@ -2,25 +2,28 @@ package logger
 
 import "time"
 
+// Options defines the configuration options for the logger.
 type Options struct {
-	Environment bool // true para "production", false para "development"
+	Environment bool // true for "production", false for "development"
 }
 
+// Field represents a log field with various data types.
 type Field interface {
-	Bool(key string, val bool) Field
-	Int(key string, val int) Field
-	Float64(key string, val float64) Field
-	String(key string, val string) Field
-	Time(key string, val time.Time) Field
-	Int64(key string, val int64) Field
+	Bool(key string, val bool) Field       // Adds a boolean field to the log entry.
+	Int(key string, val int) Field         // Adds an integer field to the log entry.
+	Float64(key string, val float64) Field // Adds a float64 field to the log entry.
+	String(key string, val string) Field   // Adds a string field to the log entry.
+	Time(key string, val time.Time) Field  // Adds a time field to the log entry.
+	Int64(key string, val int64) Field     // Adds an int64 field to the log entry.
 }
 
+// Logger provides methods for logging messages at different levels.
 type Logger interface {
-	Info(msg string, fields ...Field)
-	Error(msg string, fields ...Field)
-	Debug(msg string, fields ...Field)
-	Warn(msg string, fields ...Field)
-	Fatal(msg string, fields ...Field)
+	Info(msg string, fields ...Field)  // Logs an informational message.
+	Error(msg string, fields ...Field) // Logs an error message.
+	Debug(msg string, fields ...Field) // Logs a debug message.
+	Warn(msg string, fields ...Field)  // Logs a warning message.
+	Fatal(msg string, fields ...Field) // Logs a fatal message and terminates the application.
 
-	Field() Field
+	Field() Field // Creates a new log field for use in logging.
 }
