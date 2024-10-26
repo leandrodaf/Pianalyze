882370838e16d63e6304036bae8ed8ad15d9c6b2
Leandro Ferreira <leandro.daf@gmail.com>
2024-10-26 08:44:13 -0300

    feat(midi): add ClientMIDI interface for managing MIDI client operations
    Define an interface for MIDI operations, including device listing, selection, event capturing, and stopping the client. Added basic usage documentation.

--- internal/contracts/midi/midi.go
+++ internal/contracts/midi/midi.go
@@ -2,9 +2,10 @@ package midi
 
 import "github.com/leandrodaf/midi-client/internal/entity"
 
+// ClientMIDI defines an interface for managing MIDI client operations.
 type ClientMIDI interface {
-	Stop() error
-	ListDevices() ([]entity.DeviceInfo, error)
-	SelectDevice(deviceID int) error
-	StartCapture(eventChannel chan entity.MIDI)
+	Stop() error                                // Stops the MIDI client and disconnects any active device.
+	ListDevices() ([]entity.DeviceInfo, error)  // Lists available MIDI devices.
+	SelectDevice(deviceID int) error            // Selects a MIDI device by its ID.
+	StartCapture(eventChannel chan entity.MIDI) // Starts capturing MIDI events and sends them to the specified channel.
 }
