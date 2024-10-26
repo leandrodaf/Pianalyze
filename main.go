11e65a02861180b3142c29ec91d524c801f6c503
Leandro Ferreira <leandro.daf@gmail.com>
2024-10-26 08:49:54 -0300

    feat(main): add main entry point to start the MIDI client application
    Implement the main function to initialize the MIDI client process using cmd.Start().

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/leandrodaf/midi-client/cmd"
+
+// The main entry point for the MIDI client application.
+func main() {
+	cmd.Start() // Starts the MIDI client process.
+}
