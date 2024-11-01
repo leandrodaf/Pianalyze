7c1f9970245ef56e846b5dc204581a6aad00a988
Leandro Ferreira <leandro.dferreira@mercadolivre.com>
2024-11-01 07:47:18 -0300

    refactor(core): remove old MIDI client and pub/sub for external app integration

--- pkg/environment/environment.go
+++
@@ -1,8 +0,0 @@
-package environment
-
-import "os"
-
-// IsProduction checks if the current environment is set to production
-func IsProduction() bool {
-	return os.Getenv("GO_ENV") == "production"
-}
