2e80b328664176ed35f8a4b0391a9bf524d07f76
Leandro Ferreira <leandro.dferreira@mercadolivre.com>
2024-11-01 07:47:38 -0300

    refactor(core): remove outdated channels, contracts, and listeners

--- internal/listeners/listener.go
+++
@@ -1,8 +0,0 @@
-package listeners
-
-import "github.com/leandrodaf/pianalyze/pkg/pubsub"
-
-// Listener defines an interface for processing incoming pubsub messages.
-type Listener interface {
-	ProcessMessage(msg pubsub.Message) // Processes a received pubsub message.
-}
